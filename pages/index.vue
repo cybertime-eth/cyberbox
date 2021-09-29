67c00f3f2b4f4a9862e28d039ea44abbd70f6106
Apple <bogdans2901@icloud.com>
2021-09-29 22:59:04 +0300

    create project

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>CYBEBOX</h1>
+</template>
+
+<script>
+export default {}
+</script>
+<style>
+body {
+  display: flex;
+  justify-content: center;
+  margin: 0;
+  padding: 0;
+}
+h1 {
+  font-family: "PopinsMedium", sans-serif;
+}
+</style>
