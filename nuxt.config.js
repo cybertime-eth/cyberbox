419bc7d0ac30a83a9e67456e4164f21b4ae38e15
Richard <richardyu651@gmail.com>
2022-03-03 11:00:42 +0300

    integrate new graphql

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,7 @@ export default {
      */
     clients: {
       default: {
-        endpoint: 'https://api.thegraph.com/subgraphs/name/itdev-1210/celo-add-chin-chilla',
+        endpoint: 'https://api.thegraph.com/subgraphs/name/itdev-1210/celo-march-second',
         options: {}
       },
       secondClient: {
