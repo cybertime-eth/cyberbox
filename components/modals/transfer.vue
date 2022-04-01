4e409e1c59b6b174903f6ba9de2654bfef2a10b1
Richard <richardyu651@gmail.com>
2022-04-01 20:58:51 +0300

    update transfer flow from web & mobile

--- components/modals/transfer.vue
+++ components/modals/transfer.vue
@@ -1,21 +1,31 @@
 <template>
   <div class="modal">
-    <div class="modal__block transfer">
+    <div class="modal__block transfer" :class="{ success: successTransferToken }">
         <div class="modal__transfer-header">
           <button class="modal__transfer-header-button" @click="closeModal"><img src="/close.svg" alt="close"></button>
         </div>
-        <h2 class="modal__title">Transfer Ownership</h2>
+        <h2 class="modal__title">{{ modalTitle }}</h2>
         <div class="modal__transfer">
-          <div class="modal__transfer-info">
-            <img class="modal__image" :src="getNFTImage(nft)">
-            <p class="modal__transfer-description" :class="{ success: successTransferToken }">{{ transferDescription }}</p>
+          <div v-if="!successTransferToken">
+            <div class="modal__transfer-info">
+              <img class="modal__image" :src="getNFTImage(nft)">
+              <div class="modal__transfer-nft">
+                <p class="modal__transfer-collection">{{ collectionName }}</p>
+                <p class="modal__transfer-description">{{ transferDescription }}</p>
+              </div>
+            </div>
+            <div class="modal__transfer-form">
+              <p class="modal__transfer-form-name">Enter CELO wallet or <img class="nom-icon" src="/icon-nomdom.svg" alt="nom"> Nom domain</p>
+              <input class="modal__transfer-form-input" :readonly="successTransferToken" :placeholder="inputHint" v-model="receiver">
+              <p class="modal__transfer-form-description">You won’t be able to take back the NFT after the transaction.</p>
+            </div>
           </div>
-          <div class="modal__transfer-form">
-            <p class="modal__transfer-form-name">{{ transferFormName }}</p>
-            <input class="modal__transfer-form-input" :readonly="successTransferToken" placeholder="0x" v-model="receiver">
+          <div class="modal__transfer-success" v-else>
+            <p class="modal__transfer-description">{{ transferDescription }}</p>
+            <img class="modal__transfer-success-icon" src="/success.svg" alt="success">
+            <p class="modal__transfer-link">Viev on explorer.celo.org <img src="/share.svg" alt="share"></p>
           </div>
           <div class="modal__transfer-buttons">
-            <button class="modal__transfer-buttons-button modal__transfer-buttons-button-cancel" @click="closeModal">Cancel</button>
             <button class="modal__transfer-buttons-button modal__transfer-buttons-button-done" @click="closeModal" v-if="successTransferToken">Done</button>
             <button class="modal__transfer-buttons-button modal__transfer-buttons-button-confirm gradient-button" v-else-if="pending">
               Pending confirmation <img src="/loading-button.svg" alt="load">
@@ -34,20 +44,25 @@ export default {
     successTransferToken() {
       return this.$store.state.successTransferToken
     },
+    modalTitle() {
+      return !this.successTransferToken ? 'Transfer NFT' : 'NFT Transfered'
+    },
+    collectionName() {
+      return this.$store.state.collectionList.find(item => item.route === this.nft.contract).name
+    },
     transferDescription() {
-      if (!this.successTransferToken) {
-        const nameSuffix = this.nft.contract !== 'nomdom' ? '' : '.nom'
-        return `Transfer NFT: ${this.nft.name}${nameSuffix}`
-      } else {
-        return 'Successfully sended!'
-      }
+      const nameSuffix = this.nft.contract !== 'nomdom' ? '' : '.nom'
+      return `${this.nft.name}${nameSuffix}`
     },
     transferFormName() {
       if (!this.successTransferToken) {
-        return 'Enter CELO address'
+        return ''
       } else {
         return 'Destination address'
       }
+    },
+    inputHint() {
+      return this.isMobile() && process.browser && window.innerWidth < 767 ? 'Hold to paste' : '0x'
     }
   },
   data() {
@@ -87,15 +102,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.modal__image {
-  display: none;
+.modal__block {
+  background:  $white;
 }
-@media (max-width: 460px) {
-  .modal {
-    background: $white;
-  }
+@media(max-width: 460px) {
   .modal__title {
-    display: none;
+    width: 100%;
+    font-size: 1.8rem;
   }
 }
 </style>
@@ -104,42 +117,106 @@ export default {
 .modal {
   &__block {
     &.transfer {
-      width: 46.9rem;
-      padding: 2.46rem 1.85rem 1.85rem;
+      width: 45.6rem;
+      background: $white;
+      padding: 2.1rem 1.6rem 2.4rem;
       cursor: default;
+      &.success {
+        width: 37.6rem;
+        padding: 2.4rem 2.4rem 3.2rem;
+        .modal__title {
+          width: 100%;
+          text-align: center;
+        }
+        .modal__transfer {
+          padding-top: 0;
+          text-align: center;
+          &-header {
+            display: none;
+          }
+          &-success {
+            padding-bottom: 3.2rem;
+          }
+          &-description {
+            margin-top: 0.4rem;
+          }
+          &-success-icon {
+            padding: 2.8rem 0 2.4rem;
+          }
+          &-link {
+            font-weight: 600;
+            font-size: 1.6rem;
+            color: $grayLight;
+            img {
+              width: 2rem;
+              transform: translateY(4px);
+            }
+          }
+        }
+      }
     }
   }
   &__title {
-    font-family: Cabin-Bold;
     text-align: left;
-    font-size: 2.46rem;
+    font-size: 3.2rem;
   }
   &__transfer {
-    padding-top: 1.85rem;
+    padding-top: 1.8rem;
+    &.success {
+      padding-top: 0;
+    }
     &-header {
-      display: none;
+      display: block;
+      width: calc(100% + 3.2rem);
+      height: 1.4rem;
+      margin: 0 -1.6rem;
+      padding: 0;
+      position: relative;
+      &-button {
+        background: transparent;
+        position: absolute;
+        top: 0;
+        right: 1.6rem;
+        line-height: 1.4rem;
+      }
+    }
+    &-info {
+      display: flex;
+      flex-wrap: wrap;
+      .modal__image {
+        display: block;
+        width: 6.4rem;
+        padding: 0;
+        margin-right: 1.2rem;
+        border-radius: 4px;
+      }
+    }
+    &-collection {
+      font-weight: 600;
+      font-size: 1.4rem;
+      color: $grayLight;
     }
     &-description {
       font-weight: 600;
       font-size: 1.8rem;
-      &.success {
-        color: $green;
-      }
     }
     &-form {
       padding-top: 1.6rem;
-      padding-bottom: 5.23rem;
+      padding-bottom: 3.2rem;
       font-size: 1.8rem;
       &-name {
         padding-bottom: 1.6rem;
         font-size: 1.6rem;
+        img {
+          transform: translateY(3px);
+        }
       }
       &-input {
         width: calc(100% - 1.85rem);
         height: 1.54rem;
         padding: 0.92rem 0.78rem;
         border: 1px solid $border;
-        font-size: 1.8rem;
+        font-size: 1.6rem;
         &:focus {
           border-color: $green;
         }
@@ -149,39 +226,43 @@ export default {
           color: #9CA3AF;
         }
       }
+      &-description {
+        margin-top: 1.6rem;
+        font-size: 1.4rem;
+        color: $grayLight;
+      }
     }
     &-buttons {
       display: flex;
       justify-content: flex-end;
       &-button {
-        width: 11.1rem;
+        width: 100%;
+        background: $pink;
         padding: 1rem;
-        background: $white;
         margin-right: 1.6rem;
         border: 1px solid $pink;
         border-radius: 25px;
         text-align: center;
         font-size: 1.6rem;
-        color: $pink;
+        color: $white;
         &-confirm {
-          width: auto;
           display: flex;
           align-items: center;
+          justify-content: center;
+          background: $white;
           padding: 1rem 1.85rem;
+          color: $pink;
           cursor: default;
           img {
             margin-left: 1rem;
             animation: loading 1s infinite;
           }
         }
-        &-cancel {
-          border-color: $border;
-          color: $textColor;
-        }
         &:last-child {
           margin: 0;
         }
         &.disabled {
+          background: $white;
           pointer-events: none;
           color: $border;
           border-color: $border;
@@ -195,68 +276,39 @@ export default {
     > div:first-child {
       margin: 0 .8rem;
     }
-    &__connect {
-      &-metamask-image {
-        margin-top: 4rem;
-      }
-      &-button {
-        width: 100%;
-      }
-    }
-    &__image {
-      display: block;
-      width: 100%;
-      padding-bottom: 1.6rem;
-    }
     &__block {
       &.transfer {
         width: 100%;
-        height: calc(100% - 3.4rem);
-        background: $white;
+        height: auto;
         padding: 0 .8rem 3.2rem !important;
-        margin: 0 !important;
-        border-radius: 0;
+        border-radius: 10px;
         overflow-y: auto;
+        &.success {
+          width: 100%;
+          padding: 1.8rem 1.8rem 2.6rem !important;
+          .modal__transfer {
+            &-success-icon {
+              padding: 2.2rem 0 1.4rem;
+            }
+            &-description, &-link {
+              font-size: 1.4rem;
+            }
+          }
+        }
       }
       &-container {
         text-align: center;
       }
     }
-    &__title {
-      width: 80%;
-      margin: 0;
-      line-height: 2.2rem;
-      text-align: left;
-      font-size: 2.2rem;
-      &-domain {
-        color: $green;
-      }
-    }
     &__transfer {
-      padding-top: 0;
-      &-info {
-        display: flex;
-        flex-wrap: wrap;
-        .modal__image {
-          display: block;
-          width: 5rem;
-          padding: 0;
-          margin-right: 1rem;
-        }
-      }
+      padding-top: 1.6rem;
       &-header {
-        display: block;
         width: calc(100% + 1.6rem);
-        height: 5rem;
-        margin: 0 -0.8rem 3rem;
-        padding: 0;
-        box-shadow: 0 0 8px rgba(0, 0, 0, 0.05);
-        position: relative;
+        height: 4rem;
+        margin: 0 -0.8rem;
         &-button {
-          background: transparent;
-          position: absolute;
           top: 50%;
-          right: 0.8rem;
+          right: 1.3rem;
           transform: translateY(-50%);
         }
       }
@@ -264,11 +316,10 @@ export default {
         font-size: 1.6rem;
       }
       &-form {
-        padding-top: 2.4rem;
-        padding-bottom: 3rem;
+        padding-bottom: 2.6rem;
         &-name {
-          padding-bottom: 1.6rem;
-          font-size: 1.6rem;
+          padding-bottom: .8rem;
+          font-size: 1.4rem;
         }
         &-input {
           width: calc(100% - 2rem);
@@ -276,11 +327,13 @@ export default {
           border-color: $border2;
           font-size: 1.4rem;
         }
+        &-description {
+          font-size: 1.3rem;
+        }
       }
       &-buttons {
         &-button {
           width: 100%;
-          font-size: 1.6rem;
           &-cancel {
             display: none;
           }
