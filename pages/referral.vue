5280f16df422ea92e9e1eba55f6facb5c520a610
Richard <richardyu651@gmail.com>
2022-07-22 14:02:18 +0300

    update lending with including referral feature and bonus alert

---
+++ pages/referral.vue
@@ -0,0 +1,17 @@
+<template>
+    <section id="referral">
+        <div class="referral">
+            Referral Page
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
