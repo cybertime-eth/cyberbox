1f73280b09ba4069ea44d64d72d14155d6529544
Richard <richardyu651@gmail.com>
2022-10-13 15:19:56 +0300

    update sub description of hive collection

--- store/index.js
+++ store/index.js
@@ -98,7 +98,7 @@ export const state = () => ({
 	  discord: 'https://valoraapp.co/discord',
 	  instagram: 'https://www.instagram.com/valoraApp',
 	  description: "You don't need tokens to buy into this project; you just need to spread the word. That's how the butterfly effect and the snowball effect collide. If you believe in the power of small actions, join us on our quest to build a better future.",
-	  subDescription: "<b>Important!</b> The number of NFTs in the collection is not limited. NFT can be obtained for participating in the referral program from Valora."
+	  subDescription: "<b>Important!</b> The total number of NFTs in the collection is 2000. NFTs are issued by invitees in the referral program from Valora app."
 	},
 	{
 		id: 4,
