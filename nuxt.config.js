3c30c53ab97066f8429f1f8a505ee61a4322f7bf
Richard <richardyu651@gmail.com>
2022-10-27 12:47:31 +0300

    update graphql api

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,7 +62,7 @@ export default {
      */
     clients: {
       default: {
-        endpoint: 'https://api.thegraph.com/subgraphs/name/itdev-1210/add-hive-mind-second',
+        endpoint: 'https://api.thegraph.com/subgraphs/name/itdev-1210/exchange-bee',
         options: {}
       },
       ubeswap: {
